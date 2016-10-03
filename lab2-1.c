4c965c9ee490a0d7ce0da1f4cfcddaeed17304d8
matiasdaza <m.daza6@gmail.com>
2016-10-03 16:59:32 -0300

    Se ingresan funciones de gmp

--- lab2-1.c
+++ lab2-1.c
@@ -4,7 +4,7 @@
 #include <time.h> //Librería para medir el tiempo
 #include <gmp.h> //Librería para trabajo matemático
 
-void gcd(int a, int b);
+void gcd(mpz_t a, mpz_t b);
 
 //arcg: cantidad de parámetros contando el nombre del ejecutable que es el primer parámetro.
 //argv[]: Un arreglo que contiene todos los parámetros recibidos.
@@ -13,19 +13,19 @@ void gcd(int a, int b);
 
 main(int argc, char *argv[])
 {
-	int a, b;
+	mpz_t a, b;
 	clock_t inicio, fin; // Se crea variable 'clock_t' para calcular el tiempo de ejecúción del programa.
 	inicio=clock();
-	a = atoi(argv[1]); // Se convierten los datos ingresados a variable tipo entero.
-	b = atoi(argv[2]);
-	if(a<0 || b<0) //Condición para que los valores de n y k sean positivos!
-	{
-		printf("Los valores de a y de b deben ser positivos\n");
+	mpz_init_set_str(a,argv[1],10);
+	mpz_init_set_str(b,argv[2],10);
+	if(mpz_cmp_ui(a, 0)<0 || mpz_cmp_ui(b,0)<0) //Condición para que los valores de n y k sean positivos!
+	{	
+		printf("Los valores de n y de k deben ser positivos\n");
 		return 0;
 	}
-	if (a<b) // Como es un arreglo uno se guarda en la posición 1 y el otro en la 2.Si se ingresan mal los datos, el programa lo acusará y se cerrará.
+	if (mpz_cmp(a,b)<0) // Como es un arreglo uno se guarda en la posición 1 y el otro en la 2.Si se ingresan mal los datos, el programa lo acusará y se cerrará.
 	{
-		printf("El valor de n tiene que ser mayor que el de k.\n"); // Si se ingresan mal los datos, el programa lo acusará y se cerrará.
+		printf("El valor de a tiene que ser mayor que el de b.\n"); // Si se ingresan mal los datos, el programa lo acusará y se cerrará.
 		return 0;
 	}
 	if (argc != 3) //Como argc es la cantidad de parámetros, si lee más de 3 estará malo (porque solo necesitamos n y k)
@@ -45,22 +45,30 @@ main(int argc, char *argv[])
 	return 0; 
 }
 
-void gcd(int a, int b)
+void gcd(mpz_t a, mpz_t b)
 {
-	int r;
-
-	while(b!=0)
-	{
-		r = a-b;
-		if(r>=b)
-		{	
-			a=r;
-		}
-		else
-		{
-			a=b;
-			b=r;
-		}
-	}
-	printf("%iEl maximo comun divisor es: \n", a );
+	mpz_t resta;
+	mpz_init(resta);
+	int vueltas;
+	 while(mpz_sgn(b) != 0)  //Si b es distinto de 0, se ejecuta el ciclo while.
+    {
+        mpz_sub(resta, a, b);   //Se le asigna a la variabl....e resto la resta entre a y b.
+        if(mpz_cmp(resta, b) >= 0)  //Si resto es mayor que b, se retorna un 1, si son iguales retorna un 0 y si resto es menor que b, retorna un numero negativo.
+        {   
+            mpz_set(a, resta);  //Se le asigna a la variable a, el valor que posee resto.
+            vueltas = vueltas + 1; //Cantidad de veces que se cambia el valor de a.
+        }   
+        else    
+        {   
+            mpz_set(a, b);  //Se le asigna a la variable a, el valor de la variable b.
+            vueltas = vueltas +1;
+            mpz_set(b, resta);  //Se le asigna a b, el valor de la variable resto.
+            vueltas = vueltas + 1; //Cantidad de veces que se cambian los valores de a y b.
+        } 
+    }
+    gmp_printf("\nEl gcd es: %Zd \n", a);   //Se muestra por consola el valor del maximo comun divisor entre a y b.
+    printf("\nEl numero de veces que 'a' y/o 'b' fueron cambiados es: %d\n\n", vueltas);    //Se muestra por consola la cantidad de veces en que a y/o b fueron cambiados.mpz_sub(resto, a, b);   //Se le asigna a la variabl....e resto la resta entre a y b.
+  	mpz_clear(a);   //Se borra lo almacenado en la variable a.
+    mpz_clear(b);   //Se borra lo almacenado en la variable b.
+    mpz_clear(resta);   //Se borra lo almacenado en la variable resto.
 }
